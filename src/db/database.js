41d1af25be5cee82533f091f5a91d48a382038a6
Lucas <bazezimo42@gmail.com>
2022-07-08 01:37:23 -0300

    feat signin finished

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,7 @@
 import db  from './mongodb.js';
+//import { ObjectId } from 'mongodb';
 import bcrypt from 'bcrypt';
+//import { v4 as uuid } from 'uuid';
 
 async function createUser(user) { //finished
     try {
@@ -24,6 +26,67 @@ async function createUser(user) { //finished
     }
 };
 
+async function findUser(user) { //finished
+    try {
+        const userOnDatabase = await db.collection('users').findOne({ email: user.email });
+
+        if (!userOnDatabase) {
+          return 'not found';
+        }
+    
+        return userOnDatabase;
+
+      } catch (error) {
+        console.error(error);
+        return 'error';
+      }
+}
+
+async function newToken(user) { 
+    // código do token tirado do mywallet, precisa de revisão
+    /* 
+    const lastSession = await db.collection('tokens').findOne({userId: new ObjectId(user._id)});
+
+    const token = uuid();
+    const time = Date.now();
+
+
+
+    if(lastSession) {
+        if(time - lastSession.time < 50000 ) {
+            return lastSession;
+        }
+        try {
+            await db.collection('tokens').updateOne({userId: new ObjectId(user._id)},
+            {
+                $set: {token, time}
+            })
+            return {
+                ...lastSession,
+                token,
+                time
+            };
+        } catch (error) {
+            return 'error';
+        }
+    }
+
+    let userData = {
+        name: user.username,
+        email: user.email,
+        userId: user._id,
+        token,
+        time
+    }
+
+    await db.collection('tokens').insertOne(userData);
+
+    return userData;
+    */
+}
+
 export {
-    createUser
+    createUser,
+    findUser,
+    newToken
 };
---
+++ src/middlewares/validateSignIn.js
@@ -0,0 +1,23 @@
+import signInJoi from "../models/signInJoi.js";
+
+async function validateSignIn(req, res, next) {
+    try {
+        const user = req.body;
+        const joiValidate = await signInJoi(user);
+
+        if(!joiValidate) {
+            console.log('Falha na validação JOI signInJoi()');
+            res.status(422).send("Insira um email e senha válido!");
+            return;
+        }
+
+        res.locals.user = user;
+        next();
+        
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export default validateSignIn;
